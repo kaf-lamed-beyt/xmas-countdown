685f43ac9c50a0e34a2bb3e8fbb7011115470cb6
Caleb335 <belac335@gmail.com>
2020-02-13 21:55:29 +0100

    restructured markup and styles

--- src/app.js
+++ src/app.js
@@ -1,31 +1,31 @@
-var deadline = new Date('Feb 13, 2020 00:00:00').getTime();
-// christmas 
-function christmas() {
-    var currentDate = new Date().getTime();
-    diff = deadline - currentDate;
+// var deadline = new Date('Feb 13, 2020 00:00:00').getTime();
+// // christmas 
+// function christmas() {
+//     var currentDate = new Date().getTime();
+//     diff = deadline - currentDate;
 
-    var seconds = 1000;
-    var minutes = seconds * 60;
-    var hours = minutes * 60;
-    var days = hours * 24;
+//     var seconds = 1000;
+//     var minutes = seconds * 60;
+//     var hours = minutes * 60;
+//     var days = hours * 24;
 
-    var d = Math.floor(diff / (days));
-    var h = Math.floor((diff % (days)) / (hours));
-    var m = Math.floor((diff % (hours)) / (minutes));
-    var s = Math.floor((diff % (minutes)) / seconds);
+//     var d = Math.floor(diff / (days));
+//     var h = Math.floor((diff % (days)) / (hours));
+//     var m = Math.floor((diff % (hours)) / (minutes));
+//     var s = Math.floor((diff % (minutes)) / seconds);
 
-    document.getElementById('days').innerText = d;
-    document.getElementById('hours').innerText = h;
-    document.getElementById('minutes').innerText = m;
-    document.getElementById('seconds').innerText = s;
+//     document.getElementById('days').innerText = d;
+//     document.getElementById('hours').innerText = h;
+//     document.getElementById('minutes').innerText = m;
+//     document.getElementById('seconds').innerText = s;
 
      
-}
+// }
 
-var start = setInterval(function() {
-    christmas()
-    // Clearing the interval
-    if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-        clearInterval(start)
-    }
-}, 1000);
+// var start = setInterval(function() {
+//     christmas()
+//     // Clearing the interval
+//     if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
+//         clearInterval(start)
+//     }
+// }, 1000);
